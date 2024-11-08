ce06f25e8b41214410bd30c761f7ecbed503df3a
lx16 <3229037822@qq.com>
2024-11-08 12:42:51 +0800

    Initial commit

---
+++ MiniDouban16lx/book/admin.py
@@ -0,0 +1,10 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    search_fields = ['title', 'description']
+    list_editable = ('description',)
+
+admin.site.register(Book, BookAdmin)
